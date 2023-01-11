2fac342fbf374ce2fe4685e83ff7107f5f417524
jeremyrdavis <jeremyrdavis@mac.com>
2023-01-11 11:22:27 -0500

    terraform init working

--- src/main/java/com/ticketweasel/platformengineering/Deployer.java
+++ src/main/java/com/ticketweasel/platformengineering/Deployer.java
@@ -5,10 +5,18 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 @QuarkusMain
 public class Deployer implements QuarkusApplication {
@@ -30,17 +38,49 @@ public class Deployer implements QuarkusApplication {
 
         TicketWeaselDeployment deplyment = new TicketWeaselDeployment(ticketWeaselPlatformLocalDir);
 
+        File tmpdir = Files.createTempDirectory("ticketWeaselBuild-" + Instant.now().toString()).toFile();
+
+        // Clone the GitHub repo containing Terraform files
         try {
-            System.out.println("Cloning "+deplyment.githubTerraform+" into "+deplyment.localTerraform);
+            System.out.println("Cloning "+deplyment.githubTerraform+" into "+tmpdir);
             Git.cloneRepository()
                     .setURI(deplyment.githubTerraform)
-                    .setDirectory(Paths.get(deplyment.localTerraform).toFile())
+                    .setDirectory(tmpdir)
                     .call();
-            System.out.println("Completed Cloning");
+            System.out.println("Completed Cloning into " + tmpdir.getAbsolutePath());
         } catch (GitAPIException e) {
             System.out.println("Exception occurred while cloning repo");
             e.printStackTrace();
         }
+
+        // Execute terraform init
+//        ProcessBuilder processBuilder = new ProcessBuilder();
+//        processBuilder.directory(tmpdir);
+//        processBuilder.command("terraform init");
+        Process process = Runtime.getRuntime().exec("terraform init", null, tmpdir);//processBuilder.start();
+        StreamGobbler streamGobbler =
+                new StreamGobbler(process.getInputStream(), System.out::println);
+        Future<?> future = Executors.newSingleThreadExecutor().submit(streamGobbler);
+        int exitCode = process.waitFor();
+        assert exitCode == 0;
+        future.get(10, TimeUnit.SECONDS);
+
         return 0;
+    }
+
+    private static class StreamGobbler implements Runnable {
+        private InputStream inputStream;
+        private Consumer<String> consumer;
+
+        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines()
+                    .forEach(consumer);
+        }
     }
 }
